3c30f3ba6c07920638bc3c0361be1e8a0f11958c
Deep315 <deepmanna3151@gmail.com>
2023-09-26 02:12:01 +0530

    player not respwan

--- Assets/scripts/PlayerLife.cs
+++ Assets/scripts/PlayerLife.cs
@@ -18,7 +18,7 @@ public class PlayerLife : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("EnemyBody"))
+        if (collision.gameObject.CompareTag("EnemyBody") && collision.gameObject.CompareTag("EnemyBullet"))
         {
             Die();
         }
--- Assets/scripts/enemyBullet.cs
+++ Assets/scripts/enemyBullet.cs
@@ -6,16 +6,19 @@ public class enemyBullet : MonoBehaviour
 {
     public float killPlayer = 8;
 
+
     void Awake()
     {
         Destroy(gameObject, killPlayer);
         Physics.IgnoreLayerCollision(gameObject.layer, 9);
+        Physics.IgnoreLayerCollision(gameObject.layer, 11);
+        Physics.IgnoreLayerCollision(gameObject.layer, 6);
         Physics.IgnoreLayerCollision(gameObject.layer, gameObject.layer);
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        Destroy(collision.gameObject);
+       /* Destroy(collision.gameObject);*/
         Destroy(gameObject);
     }
 
